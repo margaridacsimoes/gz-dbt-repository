34fe7cefb459dcf0e2b4ce4a2b493caea4d829a4
margarida.cunha.simoes@gmail.com <margarida.cunha.simoes@gmail.com>
2023-11-07 15:36:27 +0000

    mart model new

---
+++ models/intermediate/finance_days.sql
@@ -0,0 +1,8 @@
+select
+COUNT (orders_id) as nb_transactions
+,SUM(revenue) as revenue_sum
+,AVG (quantity) as average_basket
+,SUM (margin) as margin_sum
+,SUM (operational_margin) as operational_margin_sum
+
+from {{ ref ('int_orders_operational')}}
