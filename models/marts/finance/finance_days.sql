97e49abe1c7e6dddc8681512722a2d6e3fe838b9
margarida.cunha.simoes@gmail.com <margarida.cunha.simoes@gmail.com>
2023-11-08 13:46:50 +0000

    finance day per day

--- models/marts/finance/finance_days.sql
+++ models/marts/finance/finance_days.sql
@@ -1,8 +1,11 @@
 select
-COUNT (orders_id) as nb_transactions
+date_date
+,COUNT (orders_id) as nb_transactions
 ,SUM(revenue) as revenue_sum
 ,AVG (quantity) as average_basket
 ,SUM (margin) as margin_sum
 ,SUM (operational_margin) as operational_margin_sum
 
 from {{ ref ('int_orders_operational')}}
+
+group by date_date
